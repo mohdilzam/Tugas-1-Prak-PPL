938d8f0ca0ba870c50fe4a3e259677ca958cc099
fazhirarizkyharmayani <fazhira_rh@mhs.usk.ac.id>
2025-02-22 20:01:13 +0700

    tambah function prima

--- solution.js
+++ solution.js
@@ -70,6 +70,11 @@ function faktorial(n) {
  * @returns {boolean}
  */
 function isPrima(n) {
+    if (n < 2) return false;
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+        if (n % i === 0) return false;
+    }
+    return true;
 }
 
 /**
