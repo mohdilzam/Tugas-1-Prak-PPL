997b8ad299571366a3e14c9b043edb2a2e9e723e
mohdilzam <mohd.ilzam60@gmail.com>
2025-02-23 19:48:47 +0700

    index.js start

---
+++ index.js
@@ -0,0 +1,8 @@
+// Aplikasi Kalkulator
+
+const math = require('./solution');
+const readline = require('readline');
+
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+
+const operations = {
