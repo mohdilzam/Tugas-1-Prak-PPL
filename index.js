cb21c25a24aa2675c448674d25ccb8610a52fa3f
mohdilzam <mohd.ilzam60@gmail.com>
2025-02-23 19:49:39 +0700

    add decl operations

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ const readline = require('readline');
 const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
 
 const operations = {
+    '1': { name: 'Penjumlahan', func: math.tambah },
+    '2': { name: 'Pengurangan', func: math.kurang },
+    '3': { name: 'Perkalian', func: math.kali },
+    '4': { name: 'Pembagian', func: math.bagi },
